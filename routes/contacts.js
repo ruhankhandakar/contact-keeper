7a1794b1e6239d119a713672f1d3e420d52a3d65
Ruhan Khandakar <khandakarruhan@gmail.com>
2019-11-05 08:42:43 +0530

    create contact

--- routes/contacts.js
+++ routes/contacts.js
@@ -28,11 +28,40 @@ router.get("/", authMiddleware, async (req, res) => {
 @desc       Add new contact
 @access     Private
 */
-router.post("/", (req, res) => {
-  res.json({
-    msg: "Add new contact"
-  });
-});
+router.post(
+  "/",
+  [
+    authMiddleware,
+    [
+      check("name", "Name is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    const { name, email, phone, type } = req.body;
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id
+      });
+
+      const contact = await newContact.save();
+
+      res.json(contact);
+    } catch (error) {
+      console.log(error.messaage);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 /* 
 @route      PUT api/contacts/:id
