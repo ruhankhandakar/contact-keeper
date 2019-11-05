74f45b64449f1285c51f8b9014b3bb62a14368d0
Ruhan Khandakar <khandakarruhan@gmail.com>
2019-11-05 07:58:28 +0530

    add jwt

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const { check, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const express = require("express");
 const router = express.Router();
 
@@ -35,7 +37,26 @@ router.post(
       const salt = await bcrypt.genSalt(10);
       user.password = await bcrypt.hash(password, salt);
       await user.save();
-      res.send("User saved");
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) {
+            return res.status(500).json(err);
+          }
+          res.json(token);
+        }
+      );
     } catch (error) {
       console.log(error.messaage);
       res.status(500).send("Server Error");
