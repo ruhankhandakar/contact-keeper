4038fca242d66ae9298947aac50697207abbba62
Ruhan Khandakar <khandakarruhan@gmail.com>
2019-11-05 10:48:38 +0530

    add bascis router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Route, Switch } from "react-router-dom";
 
 import "./App.css";
 
-function App() {
-  return <div className="App">Hello there</div>;
-}
+import Navbar from "./components/layout/Navbar";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+
+const App = () => {
+  return (
+    <Fragment>
+      <Navbar />
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+        </Switch>
+      </div>
+    </Fragment>
+  );
+};
 
 export default App;
---
+++ client/src/components/layout/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const Navbar = ({ title, icon }) => (
+  <div className="navbar bg-primary">
+    <h1>
+      <i className={icon}></i> {title}
+    </h1>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+    </ul>
+  </div>
+);
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
+};
+Navbar.defaultProps = {
+  title: "Contact Keeper",
+  icon: "fas fa-id-cart-alt"
+};
+
+export default Navbar;
