130b0e1b87ba7410ff0e39903c15b96f6d5f5550
Ruhan Khandakar <khandakarruhan@gmail.com>
2019-11-05 07:48:31 +0530

    hash password

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,5 @@
+const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 const express = require("express");
 const router = express.Router();
 
@@ -8,8 +10,37 @@ const User = require("../models/User");
 @desc       Register a user
 @access     Public
 */
-router.post("/", (req, res) => {
-  res.json(req.body);
-});
+router.post(
+  "/",
+  [
+    check("name", "name is required")
+      .not()
+      .isEmpty(),
+    check("email", "valid email is required").isEmail(),
+    check("password", "password should be greater than 5").isLength({ min: 5 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: "Authentication Failed" });
+      }
+      user = new User({ name, email, password });
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+      res.send("User saved");
+    } catch (error) {
+      console.log(error.messaage);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
