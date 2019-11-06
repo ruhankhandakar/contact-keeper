8869d51c5b5c34085db68710ea11925afbe3ecf1
Ruhan Khandakar <khandakarruhan@gmail.com>
2019-11-06 07:42:26 +0530

    add basic context state

---
+++ client/src/context/contact/contactReducer.js
@@ -0,0 +1,3 @@
+const contactReducer = () => "Hi";
+
+export default contactReducer;
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+
 import App from "./App";
+import ContactState from "./context/contact/ContactState";
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <ContactState>
+    <Router>
+      <App />
+    </Router>
+  </ContactState>,
   document.getElementById("root")
 );
