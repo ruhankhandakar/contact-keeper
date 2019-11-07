6111659385f352bc22c615ca5be3c6c7382a2bc4
Ruhan Khandakar <khandakarruhan@gmail.com>
2019-11-07 06:47:35 +0530

    setup alert context

--- client/src/index.js
+++ client/src/index.js
@@ -5,14 +5,17 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import ContactState from "./context/contact/ContactState";
 import AuthState from "./context/auth/AuthState";
+import AlertState from "./context/alert/AlertState";
 
 ReactDOM.render(
   <AuthState>
-    <ContactState>
-      <Router>
-        <App />
-      </Router>
-    </ContactState>
+    <AlertState>
+      <ContactState>
+        <Router>
+          <App />
+        </Router>
+      </ContactState>
+    </AlertState>
   </AuthState>,
   document.getElementById("root")
 );
