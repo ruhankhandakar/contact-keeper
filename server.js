ad5882a88396a51025707d741f1507c673181e62
Ruhan Khandakar <khandakarruhan@gmail.com>
2019-11-07 09:32:18 +0530

    setup for deployment

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
 
+const path = require("path");
+
 // Connect DB
 connectDB();
 
@@ -9,16 +11,20 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.json({
-    msg: "Welcome to the Contact Keeper API"
-  });
-});
-
 // Define routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+// Server Static assests in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.relative(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Start server
 const PORT = process.env.PORT || 5000;
